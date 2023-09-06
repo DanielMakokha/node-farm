6a9146b49790ec2d86edf4faaa6f4269f5f337b4
DanielMakokha <danielmakokhan@gmail.com>
2023-09-06 19:49:46 +0300

    writing files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,10 @@
 const fs = require('fs')
-const hello = "Hello world";
 
-console.log(hello);
+const data=fs.readFileSync('./txt/input.txt','utf-8');
+
+console.log(data);
+
+const textOut=`This is how we write data to a new file.${data}`;
+fs.writeFileSync('./txt/output.txt',textOut)
+
+console.log('File written');
