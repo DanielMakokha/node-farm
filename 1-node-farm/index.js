2f316567eaf7df839612f991dc72dad1c4a3bdb9
DanielMakokha <danielmakokhan@gmail.com>
2023-09-07 06:48:55 +0300

    urls

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
+const http= require('http')
+const url=require('url')
+
+////////////////////
+//FILE
 
 // const data=fs.readFileSync('./txt/input.txt','utf-8');
 
@@ -11,16 +16,45 @@ const fs = require('fs')
 
 //Non-Blocking asynchronous code
 
-fs.readFile('./txt/start.txt','utf-8',(err,data1) =>{
-    console.log(data1);
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) =>{
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`,'utf-8',(err,data3) =>{
-            console.log(data3);
-            fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`,'utf-8',(err) =>{
-                console.log('Data has been saved');
-            });
-        });
-    });
-    });
-console.log('File has been read');
+// fs.readFile('./txt/start.txt','utf-8',(err,data1) =>{
+//     console.log(data1);
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) =>{
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`,'utf-8',(err,data3) =>{
+//             console.log(data3);
+//             fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`,'utf-8',(err) =>{
+//                 console.log('Data has been saved');
+//             });
+//         });
+//     });
+//     });
+// console.log('File has been read');
+
+
+//////////////////////
+////SERVER
+
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8') 
+const productData=JSON.parse(data)
+
+const server=http.createServer((req,res) =>{
+    const pathName=req.url;
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview');
+    }else if(pathName === '/product'){
+        res.end('This is product');
+    }else if(pathName === '/api'){
+        res.writeHead(200,{'Content-type':'application/json'});
+        res.end(data);
+        
+    }else{
+        res.writeHead(404,{
+            'Content-type':'text/html'
+        })
+        res.end('<h1>page not found</h1>')
+    }
+})
+
+server.listen(8000,'127.0.0.1',() =>{
+    console.log('Server is listening incoming requests');
+})
