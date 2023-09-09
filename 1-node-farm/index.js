7c24bdac3db6f5a6c5bd6c959ca7c18a38c81f7e
DanielMakokha <danielmakokhan@gmail.com>
2023-09-09 08:05:38 +0300

    part 2 render

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -52,23 +52,30 @@ const data=fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj=JSON.parse(data)
 
 const server=http.createServer((req,res) =>{
-    const pathName=req.url;
+    
+    const { query, pathname } = url.parse(req.url, true);
+
+
 
     // OVERVIEW PAGE
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200,{'Content-type':'text/html'});
-       
-
         const cardsHtml=dataObj.map(el => replaceTemplate(tempCard,el)).join('');
         const output=tempOverview.replace(`{%PRODUCT_CARDS%}`,cardsHtml)
         res.end(output);
 
+
     // PRODUCT PAGE
-    }else if(pathName === '/product'){
-        res.end('This is product');
+    }else if(pathname === '/product'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const product=dataObj[query.id];
+        const output=replaceTemplate(tempProduct,product)
+        console.log(query);
+        res.end(output);
+        
 
     // API PAGE
-    }else if(pathName === '/api'){
+    }else if(pathname === '/api'){
         res.writeHead(200,{'Content-type':'application/json'});
         res.end(data);
         
